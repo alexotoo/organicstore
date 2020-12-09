f26c3f44fff210664159fa190fce6df4df8d4f6a
Alex Otoo <alexotoous@outlook.com>
2020-12-09 17:05:43 +0000

    product fronend done

--- organicstore-client/src/App.js
+++ organicstore-client/src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomePage from "./Pages/HomePage";
+import ProductPage from "./Pages/ProductPage";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <Router>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/Product/:id" component={ProductPage} />
+        </main>
+      </Container>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ organicstore-client/src/Pages/ProductPage.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductPage = ({ match }) => {
+  const product = products.find((product) => product._id === match.params.id);
+  console.log(product);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid alt />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>Price: $ {product.price}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>{product.description}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews}   reviews`}
+              />
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>price:</Col>
+                  <Col>
+                    <strong>${product.price} </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductPage;
