b945d1fe71a49dd096f005426b8e4342d46f043d
Alex Otoo <alexotoous@outlook.com>
2020-12-10 12:37:51 +0000

    model/schema done

---
+++ server/model/productModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const reviewschema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comments: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Boolen,
+      required: true,
+    },
+    description: {
+      type: Boolen,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInstock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product = mongoose.model("Product", userSchema);
+
+export default Product;
