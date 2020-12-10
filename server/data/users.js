d7280d8d3401ce094b4dee2764a9fd05f2f6bc37
Alex Otoo <alexotoous@outlook.com>
2020-12-10 14:49:38 +0000

    schema changedv1

---
+++ server/data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "admin user",
+    email: "examplesadmin@try.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Ama Esi",
+    email: "exampeama@try.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Kofi Sam",
+    email: "kofimandmin@try.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
