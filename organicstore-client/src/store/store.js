7c0b550ceb0e5451322a744183e8e006633a5487
Alex Otoo <alexotoous@outlook.com>
2021-01-02 11:36:25 +0000

    user login reducer,action done

--- organicstore-client/src/store/store.js
+++ organicstore-client/src/store/store.js
@@ -4,19 +4,26 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 import * as productReducers from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productReducers.productListReducer,
   productDetails: productReducers.productDetailReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsInStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoInStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsInStorage },
+  userLogin: userInfoInStorage,
 };
 
 const middleware = [thunk];
